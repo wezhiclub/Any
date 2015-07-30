3728b891aabcd0bf99ae156bb07bec325fe08e4d
shuzihao <zhshu@incardata.com.cn>
2015-07-30 17:31:59 +0800

    新建了一个方法！

--- Abc.java
+++ Abc.java
@@ -3,4 +3,8 @@ public class Abc
 	public static void main(String[] args){
 		System.out.println("Hello");
 	}
+
+    public void addUser(){
+        System.out.println("Hello Mr Xiong!");
+    }
 };
