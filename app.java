be65b9481e328a7533cba46eae8f4443d4e67d77
AceBear <vast_m_bear@yahoo.com>
2015-07-30 17:02:38 +0800

    fix a sdfdf

--- app.java
+++ app.java
@@ -6,7 +6,7 @@ import java.lang;
 public class Any{
 	public static void main(String[] args){
 		// added by jml 2015-7-30
-		System.out.println("I'm a beauty");
+		System.out.println("I'm a beauty.");
 	}
 	
 	public void foo(){}
